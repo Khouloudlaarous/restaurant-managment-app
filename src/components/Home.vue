347ab3a102229641589d1a09cfdfea862fbfd40c
Khouloudlaarous <khouloudaissa@gmail.com>
2023-06-16 00:42:08 +0100

    add list of restaurants with api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,40 @@
 <template>
   <Header/>
-  <h1 class="home">Home Page</h1>
-  <img src="../assets/background.jpg">
+  <h1 class="home">Hello {{name}}, Welcome on our Page </h1>
+  <table border="1">
+    <tr v-for="item in restaurants" :key="item.id">
+      <td>{{item.id}}</td>
+      <td>{{item.name}}</td>
+      <td>{{item.address}}</td>
+    </tr>
+  </table>
 </template>
 <script>
 import Header from "@/components/Header.vue";
+import axios from "axios";
 export default {
   name:`Home`,
+  data (){
+    return {
+      name:'',
+      restaurants: [],
+    }
+  },
   components:{
     Header
   },
-  // mounted()
-  // {
-  //   let user= localStorage.getItem('user-info');
-  //   if(!user)
-  //   {
-  //     this.$router.push({name:'SignUp'})
-  //   }
-  // }
+  async mounted()
+  {
+    let user= localStorage.getItem('user-info');
+    this.name=JSON.parse(user).name;
+    if(!user)
+    {
+      this.$router.push({name:'SignUp'})
+    }
+    let result =await axios.get("http://localhost:3000/user");
+    console.warn(result)
+    this.restaurants=result.data;
+  }
 }
 </script>
 <style>
